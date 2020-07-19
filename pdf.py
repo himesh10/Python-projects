2eb847aa95851705d0d82834330133a00f00b001
himesh10 <himesh10@hotmail.co.uk>
2020-07-20 00:17:51 +0100

    committing pdf.py to repository

---
+++ pdf.py
@@ -0,0 +1,43 @@
+import PyPDF2
+#import sys
+
+# watermark ur pdfs
+
+template = PyPDF2.PdfFileReader(open("super.pdf", 'rb'))
+watermark = PyPDF2.PdfFileReader(open("wtr.pdf", 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+	page = template.getPage(i)
+	page.mergePage(watermark.getPage(0))
+	output.addPage(page)
+
+	with open("watermarked_output.pdf", "wb") as file:
+		output.write(file)
+
+
+# merging pdfs; pdf combiner - need import sys
+
+
+# inputs = sys.argv[1:]
+
+# def pdf_combine(pdf_list):
+# 	merger = PyPDF2.PdfFileMerger()
+# 	for pdf in pdf_list:
+# 		print(pdf)
+# 		merger.append(pdf)
+# 	merger.write("super.pdf")
+
+# pdf_combine(inputs)
+
+
+#### tilts pdf
+
+# with open("dummy.pdf", 'rb') as file:
+# 	reader = PyPDF2.PdfFileReader(file)
+# 	page = reader.getPage(0)
+# 	page.rotateCounterClockwise(90)
+# 	writer = PyPDF2.PdfFileWriter()
+# 	writer.addPage(page)
+# 	with open("tilt.pdf", 'wb') as new_file:
+# 		writer.write(new_file)
